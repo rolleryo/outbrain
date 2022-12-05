4e5994f846e099781173a11d10d7277e8be4c304
rolleryo <yoavroller@gmail.com>
2022-12-06 00:29:04 +0200

    format response as json

--- task1.py
+++ task1.py
@@ -1,18 +1,26 @@
 import ipinfo
-# requests library:  http://docs.python-requests.org/en/latest/
+import requests #http://docs.python-requests.org/en/latest/
 from requests import get
-import requests
 import json
+# get external ip using http request from ipify
+external_ip = get('https://api.ipify.org').content.decode('utf8')
 
-#ip_address = get('https://api.ipify.org').content.decode('utf8')
-#print('My public IP address is: {}'.format(ip))
-
+# get city & country according to external ip using ipinfo library
 ipinfo_token = 'd5a13af3bebba0'
-handler = ipinfo.getHandler(ipinfo_token)
-ip_address = ip = get('https://api.ipify.org').content.decode('utf8')
-ipinfo_details = handler.getDetails(ip_address)
+handler = ipinfo.getHandler(ipinfo_token) # create object handler 
+ipinfo_details = handler.getDetails(external_ip) # use getDetails property
 city = ipinfo_details.city
 country = ipinfo_details.country
+# get temp with respect to city & country 
 weatherReq = requests.get('http://api.openweathermap.org/data/2.5/weather?units=metric&q='+city+','+country+'&APPID=0a1e14ce198f208c5665f1b1a6bb4879').text
+# parse response using json
 data = json.loads(weatherReq)
-print(data['main']['temp'])
+temp=data['main']['temp']
+# format response as json 
+value = {
+        "city": city,
+        "country": country,
+        "temp": temp,
+        }
+jsonString = json.dumps(value)
+print(jsonString)
