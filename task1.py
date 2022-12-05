9e81e2f7012aca00f56f1a40ba80b201ff5ceb15
rolleryo <yoavroller@gmail.com>
2022-12-05 23:08:30 +0200

    get city and country using ipinfo library

--- task1.py
+++ task1.py
@@ -1,7 +1,14 @@
-# This example requires the requests library be installed.  You can learn more
-# about the Requests library here: http://docs.python-requests.org/en/latest/
+import ipinfo
+# requests library:  http://docs.python-requests.org/en/latest/
 from requests import get
+import requests
 
-ip = get('https://api.ipify.org').content.decode('utf8')
-print('My public IP address is: {}'.format(ip))
+#ip_address = get('https://api.ipify.org').content.decode('utf8')
+#print('My public IP address is: {}'.format(ip))
 
+ipinfo_token = 'd5a13af3bebba0'
+handler = ipinfo.getHandler(ipinfo_token)
+ip_address = ip = get('https://api.ipify.org').content.decode('utf8')
+ipinfo_details = handler.getDetails(ip_address)
+city = ipinfo_details.city
+country = ipinfo_details.country
